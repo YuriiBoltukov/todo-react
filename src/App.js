c20815408815034fd4e06ddbe265b90aeb5908bd
YuriiBoltukov <kolomiha898@gmail.com>
2022-07-27 10:54:32 +0300

    react-to-do

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
 import { useState } from 'react'
-import ToDo from './ToDo';
-import ToDoForm from './ToDoForm';
+import ToDo from './ToDo'
+import ToDoForm from './ToDoForm'
 
 function App() {
-  const [todos,setTodos] = useState([])
+  const [todos, setTodos] = useState([])
 
-  const addTask = () => {
-
+  const addTask = (userInput) => {
+    if(userInput) {
+      const newItem = {
+        id: Math.random().toString(36).substr(2,9),
+        task: userInput,
+        complete: false
+      }
+      setTodos([...todos, newItem])
+    }
   }
 
-  const removeTask = () => {
-
+  const removeTask = (id) => {
+    setTodos([...todos.filter((todo) => todo.id !== id)])
   }
 
-  const handleToggle = () => {
-
+  const handleToggle = (id) => {
+    setTodos([
+      ...todos.map((todo) => 
+        todo.id === id ? { ...todo, complete: !todo.complete } : {...todo }
+      )
+    ])
   }
 
   return (
     <div className="App">
       <header>
-        <h1>To-Do List: {todos.length}</h1>
+        <h1>Список задач: {todos.length}</h1>
       </header>
-      <ToDoForm />
+      <ToDoForm addTask={addTask} />
       {todos.map((todo) => {
         return (
-          <ToDo key={todo.id}/>
+          <ToDo
+            todo={todo}
+            key={todo.id}
+            toggleTask={handleToggle}
+            removeTask={removeTask}
+            />
         )
       })}
     </div>
